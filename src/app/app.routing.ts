0671e1948dcf2126964b52e387c74a4ad6fdff0c
Jorge Jimenez <george@shocklogic.com>
2017-04-13 19:01:08 -0400

    Added home andprofile componenet

---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import { ProfileComponent } from './profile/profile.component';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent
+    }
+];
+
+export const appRoutingProviders: any[] = [];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
